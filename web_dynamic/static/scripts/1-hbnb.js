b3396202d426a89208c94e4b83e937770a0581b2
Martin Mulama <martin.mulama@yahoo.com>
2023-10-05 17:46:33 +0000

    added 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,17 @@
-$(document). ready()
+let amenity_store = {}; // Initialize an empty object to store amenities
+
+$(document).ready(function() {
+  $("input[type=checkbox]").click(function () {
+    if (this.checked) {
+      amenity_store[$(this).data('id')] = $(this).data('name'); // Add amenity to the storing object
+      $(".amenities h4").text(Object.values(amenity_store).join(', ')); // Update the h4 tag with selected amenities
+    } else {
+      delete amenity_store[$(this).data('id')]; // Remove amenity from the storing object
+      if (Object.values(amenity_store).length === 0) {
+        $(".amenities h4").html("&nbsp;"); // If no amenities are selected, display an empty space
+      } else {
+        $(".amenities h4").text(Object.values(amenity_store).join(', ')); // Update the h4 tag with remaining selected amenities
+      }
+    }
+  });
+};
